183af7c3674e26eb9e9cdfeede7510674d7c2b92
tcs17181 <tcs17181@tcs-mac-1607.local>
2018-09-27 11:52:36 +0900

    スタートボタン、メニュー画面の追加＋初期化

--- irobare/GameViewController.swift
+++ irobare/GameViewController.swift
@@ -15,21 +15,15 @@ class GameViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        if let view = self.view as! SKView? {
-            // Load the SKScene from 'GameScene.sks'
-            if let scene = SKScene(fileNamed: "GameScene") {
-                // Set the scale mode to scale to fit the window
-                scene.scaleMode = .aspectFill
-                
-                // Present the scene
-                view.presentScene(scene)
-            }
-            
-            view.ignoresSiblingOrder = true
-            
-            view.showsFPS = true
-            view.showsNodeCount = true
-        }
+        let bg = UIImageView(frame: CGRect(x: 0, y: 0, width: self.view.frame.size.width, height: self.view.frame.size.height))
+        bg.image = UIImage(named: "背景menu.png")
+        bg.layer.zPosition = -1
+        self.view.addSubview(bg)
+        let scene = GameScene(size:CGSize(width: 750,height: 1334))
+        let skView = self.view as! SKView
+        scene.scaleMode = .aspectFit
+        skView.presentScene(scene)
+        
     }
 
     override var shouldAutorotate: Bool {
